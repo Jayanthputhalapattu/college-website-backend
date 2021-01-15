126130538529da89a4b4100faab9d3f5bb430819
Jayanth <jayanthp17@gmail.com>
2021-01-15 18:03:40 +0530

    cors

--- express/server.js
+++ express/server.js
@@ -31,7 +31,7 @@ router.post('/', (req, res) => res.json({ postBody: req.body }));
 app.use("/news",NewsRouter)
 app.use("/auth",Auth)
 app.use(bodyParser.json());
-
+app.use(cors())
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
